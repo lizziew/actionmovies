ba5c52cfddf89d09f0a999df5634dd3b08bbaa86
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2016-02-24 14:15:00 -0500

    exhibit framework example

---
+++ actionmovies.js
@@ -0,0 +1,124 @@
+{
+ "items":[
+  {"id":"Star Wars Episode VII - The Force Awakens",
+   "label": "Star Wars Episode VII - The Force Awakens",
+   "year":"2015",
+   "gross":"922",
+   "rating":"pg13"
+  },
+  {"id":"Avatar",
+   "label": "Avatar",
+   "year":"2009",
+   "gross":"761",
+   "rating":"pg13"
+  },
+  {"id":"Jurassic World",
+   "label": "Jurassic World",
+   "year":"2015",
+   "gross":"652",
+   "rating":"pg13"
+  },
+  {"id":"The Avengers",
+   "label": "The Avengers",
+   "year":"2012",
+   "gross":"623",
+   "rating":"pg13"
+  },
+  {"id":"The Dark Knight",
+   "label": "The Dark Knight",
+   "year":"2008",
+   "gross":"533",
+   "rating":"pg13"
+  },
+  {"id":"Star Wars Episode I - The Phantom Menace",
+   "label": "Star Wars Episode I - The Phantom Menace",
+   "year":"1999",
+   "gross":"475",
+   "rating":"pg"
+  },
+  {"id":"Star Wars",
+   "label":"Star Wars",
+   "year":"1977",
+   "gross":"461",
+   "rating":"pg"
+  },
+  {"id":"Avengers: Age of Ultron",
+   "label":"Avengers: Age of Ultron",
+   "year":"2015",
+   "gross":"459",
+   "rating":"pg13"
+  },
+  {"id":"The Dark Knight Rises",
+   "label":"The Dark Knight Rises",
+   "year":"2012",
+   "gross":"448",
+   "rating":"pg13"
+  },
+  {"id":"Pirates of the Caribbean: Dead Man's Chest",
+   "label":"Pirates of the Caribbean: Dead Man's Chest",
+   "year":"2006",
+   "gross":"423",
+   "rating":"pg13"
+  },
+  {"id":"Iron Man 3",
+   "label":"Iron Man 3",
+   "year":"2013",
+   "gross":"409",
+   "rating":"pg13"
+  },
+  {"id":"Spider-Man",
+   "label":"Spider-Man",
+   "year":"2002",
+   "gross":"404",
+   "rating":"pg13"
+  },
+  {"id":"Transformers: Revenge of the Fallen",
+   "label":"Transformers: Revenge of the Fallen",
+   "year":"2009",
+   "gross":"402",
+   "rating":"pg13"
+  },
+  {"id":"Star Wars Episode III - Revenge of the Sith",
+   "label":"Star Wars Episode III - Revenge of the Sith",
+   "year":"2005",
+   "gross":"380",
+   "rating":"pg13"
+  },
+  {"id":"Spider-Man 2",
+   "label":"Spider-Man 2",
+   "year":"2004",
+   "gross":"373",
+   "rating":"pg13"
+  },
+  {"id":"Transformers: Dark of the Moon",
+   "label":"Transformers: Dark of the Moon",
+   "year":"2011",
+   "gross":"352",
+   "rating":"pg13"
+  },
+  {"id":"American Sniper",
+   "label":"American Sniper",
+   "year":"2014",
+   "gross":"350",
+   "rating":"r"
+  },
+  {"id":"Furious 7",
+   "label":"Furious 7",
+   "year":"2015",
+   "gross":"350",
+   "rating":"pg13"
+  },
+  {"id":"Spider-Man 3",
+   "label":"Spider-Man 3",
+   "year":"2007",
+   "gross":"337",
+   "rating":"pg13"
+  },
+  {"id":"Guardians of the Galaxy",
+   "label":"Guardians of the Galaxy",
+   "year":"2014",
+   "gross":"333",
+   "rating":"pg13"
+  }
+ ]
+}
